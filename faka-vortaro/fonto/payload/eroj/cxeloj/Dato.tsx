16cddeef0000dd45b76df826412ff587eb7a8546
Behilam <praehil@gmail.com>
2024-05-16 15:49:35 -0500

    Formatigi datojn kiel DD MM YYYY

--- faka-vortaro/fonto/payload/eroj/cxeloj/Dato.tsx
+++ faka-vortaro/fonto/payload/eroj/cxeloj/Dato.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 const Dato = ({ cellData, collection, link, rowData }: Props) => {
-  const dato = <div>{dayjs(cellData).format("MMM DD, YYYY")}</div>;
+  const dato = <div>{dayjs(cellData).format("DD MM YYYY")}</div>;
   return link ? <a href={`/admin/collections/${collection.slug}/${rowData.id}`}>{dato}</a> : dato;
 };
 
